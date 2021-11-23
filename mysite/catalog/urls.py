f079556d359b87495f691d752fc3f7ab0c5f7bee
balu616 <kalikiri616@gmail.com>
2021-11-23 17:17:54 +0530

    third commit

--- mysite/catalog/urls.py
+++ mysite/catalog/urls.py
@@ -10,4 +10,6 @@ path('authors/', views.AuthorListView.as_view(), name='authors'),
 #path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
 path('author/<int:pk>', views.AuthorDetailView1, name='author-detail'),
 path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+path('borrowed/', views.allBorrowedBooksByUserListView.as_view(), name='borrowed'),
+path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
 ]
--- mysite/catalog/views.py
+++ mysite/catalog/views.py
@@ -2,7 +2,7 @@ from django.db.models.fields import NullBooleanField
 from django.http.response import Http404
 from django.shortcuts import get_object_or_404, render
 from django.views import generic
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
 # Create your views here.
 from .models import Book, Author, BookInstance, Genre
@@ -105,6 +105,58 @@ class LoanedBooksByUserListView(LoginRequiredMixin,generic.ListView):
     def get_queryset(self):
         return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
 
+
+class allBorrowedBooksByUserListView(LoginRequiredMixin,PermissionRequiredMixin,generic.ListView):
+    """Generic class-based view listing books on loan to current user."""
+    permission_required = 'accounts.action_all'
+    model = BookInstance
+    template_name ='catalog/allBorrowedBooksByUserList.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(status__exact='o').order_by('due_back')
+
+import datetime
+
+from django.contrib.auth.decorators import login_required, permission_required
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+from catalog.forms import RenewBookForm
+
+@login_required
+@permission_required('catalog.can_mark_returned', raise_exception=True)
+def renew_book_librarian(request, pk):
+    """View function for renewing a specific BookInstance by librarian."""
+    book_instance = get_object_or_404(BookInstance, pk=pk)
+
+    # If this is a POST request then process the Form data
+    if request.method == 'POST':
+
+        # Create a form instance and populate it with data from the request (binding):
+        form = RenewBookForm(request.POST)
+
+        # Check if the form is valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required (here we just write it to the model due_back field)
+            book_instance.due_back = form.cleaned_data['renewal_date']
+            book_instance.save()
+
+            # redirect to a new URL:
+            return HttpResponseRedirect(reverse('borrowed') )
+
+    # If this is a GET (or any other method) create the default form.
+    else:
+        proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
+        form = RenewBookForm(initial={'renewal_date': proposed_renewal_date})
+
+    context = {
+        'form': form,
+        'book_instance': book_instance,
+    }
+
+    return render(request, 'catalog/book_renew_librarian.html', context)
 
 #if the record doesnot exist then the following code handles that and gives error
 def book_detail_view(request, pk):
