8e2357445b9e1ad46557e9fcf4db601bc02a6a19
balu616 <kalikiri616@gmail.com>
2021-12-01 10:24:16 +0530

    tests

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -21,12 +21,22 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5o2+nzvytw6oz08nf7xh5%8c7vfi&d6d#hu^+n1$pk(es$tl2r'
+#SECRET_KEY = 'django-insecure-5o2+nzvytw6oz08nf7xh5%8c7vfi&d6d#hu^+n1$pk(es$tl2r'
+
+#production
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG =False
 
-ALLOWED_HOSTS = []
+#production
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+ALLOWED_HOSTS = ['localhost','192.168.57.190']
 
 
 # Application definition
